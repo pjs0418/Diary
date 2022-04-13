4d235bb1a365bf119cdfa00fb78078938548a022
pjs0418 <tony180322@gmail.com>
2022-04-13 13:53:07 +0900

    feat: Draw UI for writing diary

---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 준수의 MacBook on 2022/04/13.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
