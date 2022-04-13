b28830b0d3f11f9803fb07004ecd2775b2038890
pjs0418 <tony180322@gmail.com>
2022-04-13 14:10:09 +0900

    feat: Draw UI for diary detail info screen

---
+++ Diary/DiaryDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 준수의 MacBook on 2022/04/13.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
