243ec6efcc19432754f94f4b9a6e24f83a6d619e
pjs0418 <tony180322@gmail.com>
2022-04-15 12:16:04 +0900

    feat: Implement function for checking whether all inputs are written

--- Diary/ViewController.swift
+++ Diary/ViewController.swift
@@ -8,12 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
