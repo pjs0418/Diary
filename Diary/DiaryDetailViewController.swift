261f645ccb291c8464aedbcde692449bb8211916
pjs0418 <tony180322@gmail.com>
2022-04-17 11:39:18 +0900

    feat: Change indexPath to UUID and complete project

--- Diary/DiaryDetailViewController.swift
+++ Diary/DiaryDetailViewController.swift
@@ -19,6 +19,7 @@ class DiaryDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureView()
+        NotificationCenter.default.addObserver(self, selector: #selector(starDiaryNotification(_:)), name: NSNotification.Name("starDiary"), object: nil)
     }
     
     private func configureView() {
@@ -41,7 +42,6 @@ class DiaryDetailViewController: UIViewController {
     
     @objc func tapStarButton() {
         guard let isStar = self.diary?.isStar else { return }
-        guard let indexPath = self.indexPath else { return }
         if isStar {
             self.starButton?.image = UIImage(systemName: "star")
         } else {
@@ -51,7 +51,7 @@ class DiaryDetailViewController: UIViewController {
         NotificationCenter.default.post(name: NSNotification.Name("starDiary"), object: [
             "diary": self.diary,
             "isStar": self.diary?.isStar ?? false,
-            "indexPath": indexPath
+            "uuidString": self.diary?.uuidString
         ], userInfo: nil)
     }
     
@@ -65,15 +65,25 @@ class DiaryDetailViewController: UIViewController {
     }
     
     @objc func editDiaryNotification(_ notification: Notification) {
-        guard let diary  = notification.object as? Diary else { return }
-        guard let row = notification.userInfo?["indexPath.row"] as? Int else { return }
+        guard let diary = notification.object as? Diary else { return }
         self.diary = diary
         self.configureView()
     }
     
+    @objc func starDiaryNotification(_ notification: Notification) {
+        guard let starDiary = notification.object as? [String: Any] else { return }
+        guard let isStar = starDiary["isStar"] as? Bool else { return }
+        guard let uuidString = starDiary["uuidString"] as? String else { return }
+        guard let diary = self.diary else { return }
+        if diary.uuidString == uuidString {
+            self.diary?.isStar = isStar
+        }
+        self.configureView()
+    }
+    
     @IBAction func tapDeleteButton(_ sender: UIButton) {
-        guard let indexPath = self.indexPath else { return }
-        NotificationCenter.default.post(name: NSNotification.Name("deleteDiary"), object: indexPath, userInfo: nil)
+        guard let uuidString = self.diary?.uuidString else { return }
+        NotificationCenter.default.post(name: NSNotification.Name("deleteDiary"), object: uuidString, userInfo: nil)
         self.navigationController?.popViewController(animated: true)
     }
     
--- Diary/WriteDiaryViewController.swift
+++ Diary/WriteDiaryViewController.swift
@@ -105,13 +105,11 @@ class WriteDiaryViewController: UIViewController {
         
         switch self.diaryEditorMode {
         case .new:
-            let diary = Diary(title: title, contents: contents, date: date, isStar: false)
+            let diary = Diary(uuidString: UUID().uuidString, title: title, contents: contents, date: date, isStar: false)
             self.delegate?.didSelectRegister(diary: diary)
-        case let .edit(indexPath, diary):
-            let diary = Diary(title: title, contents: contents, date: date, isStar: diary.isStar)
-            NotificationCenter.default.post(name: NSNotification.Name("editDiary"), object: diary, userInfo: [
-                "indexPath.row": indexPath.row
-            ])
+        case let .edit(_, diary):
+            let diary = Diary(uuidString: diary.uuidString, title: title, contents: contents, date: date, isStar: diary.isStar)
+            NotificationCenter.default.post(name: NSNotification.Name("editDiary"), object: diary, userInfo: nil)
         }
         
         self.navigationController?.popViewController(animated: true)
